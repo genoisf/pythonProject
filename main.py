d5e3b1cf089caf9632c3e65254d2b77fa974b848
genoisf <genoisf@educ.college-montreal.qc.ca>
2023-11-15 11:09:09 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,34 @@
+#créé par Francis Genois
+#créé en 2023
+#tp4 introduction a l'arcade
+
+import arcade*
+import random
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+COLORS = []
+
+
+class MyGame(arcade.Window):
+    def __init__(self):
+        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, "Exercice #1")
+        pass
+
+    def setup(self):
+        pass
+
+    def on_draw(self):
+        arcade.start_render()
+
+        pass
+
+
+def main():
+    my_game = MyGame()
+    my_game.setup()
+
+    arcade.run()
+
+main()
